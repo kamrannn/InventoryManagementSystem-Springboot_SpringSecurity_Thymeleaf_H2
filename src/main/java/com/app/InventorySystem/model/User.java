8b30325ef36bf9f3bb27dcb0b0babdd20f20f4b0
kamranabbasi <sendtokamran0@gmail.com>
2022-01-24 02:08:56 +0500

    Tooltip for the choose file is also added.

--- src/main/java/com/app/InventorySystem/model/User.java
+++ src/main/java/com/app/InventorySystem/model/User.java
@@ -1,12 +1,9 @@
 package com.app.InventorySystem.model;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
-@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -23,4 +20,63 @@ public class User {
     @NotBlank(message = "email is mandatory")
     @Column(unique = true)
     private String email;
+
+    public User() {
+    }
+
+    public User(@NotBlank(message = "full name is mandatory") String fullName, @NotBlank(message = "username is mandatory") String username, @NotBlank String password, String role, @NotBlank(message = "email is mandatory") String email) {
+        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
